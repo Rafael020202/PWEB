5dc11d54ec61d087725b57b268513f9854c78d22
rafael020202 <luizrafael391@gmail.com>
2020-06-01 12:08:09 -0300

    atividade 9

---
+++ Atividade9/utils/events.js
@@ -0,0 +1,9 @@
+function toUpper() {
+  var txt = document.getElementById('txt').value;
+  document.getElementById('txt').value = txt.toUpperCase();
+}
+
+function toLower() {
+  var txt = document.getElementById('txt').value;
+  document.getElementById('txt').value = txt.toLowerCase(); 
+}
